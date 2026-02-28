1e6a26316b0b640dad1039eea7c1e0728df56430
igoryava <126491702+igoryava@users.noreply.github.com>
2026-03-01 00:19:32 +0700

    added global score

--- Assets/Source/Score/Score.cs
+++ Assets/Source/Score/Score.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Zenject;
 
@@ -8,13 +9,27 @@ public class Score : IInitializable
     public int CurrentValue { get; private set; }
     private ScoreConfig _config;
     private IScoreViewable _scoreViewable;
+    private Hand _hand;
+    private CombinationContainer _combinationContainer;
     
-    public Score(ScoreConfig config, IScoreViewable scoreViewable)
+    public Score(ScoreConfig config, IScoreViewable scoreViewable, Hand hand, CombinationContainer combinationContainer)
     {
         _config = config;
         _scoreViewable = scoreViewable;
+        _hand = hand;
+        _combinationContainer = combinationContainer;
         CurrentValue = _config.StartValue;
+        _hand.PlayHand += OnHandPlayed;
         _scoreViewable.ChangeVisual(CurrentValue);
+    }
+
+    private void OnHandPlayed(CardVisual[] cardVisuals)
+    {
+        Combination combination = cardVisuals.ToList().GetBestCombination(_combinationContainer.CombinationsConfig);
+        for (int i = 0; i < cardVisuals.Length; i++)
+        {
+            Add(cardVisuals[i].Card.Rank.GetCardValue() * combination.Multiplier);
+        }
     }
 
     public void Add(int addValue)
